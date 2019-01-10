fe7fe2d83d7fadbfa1bad32cb4474bf58ef534c0
JerryRun <565636992@qq.com>
2019-01-10 23:39:11 +0800

    10.4 5 6

---
+++ lesson10/src/test/java/com/lr/mule/lesson10/HttpsServerTestCase.java
@@ -0,0 +1,14 @@
+package com.lr.mule.lesson10;
+
+
+
+public class HttpsServerTestCase extends AbstractConfigurationLoaderTestCase 
+{
+
+	@Override
+	protected String getConfigResources() 
+	{
+		return "4.https-server.xml";
+	}
+	
+}
